df850b16811b477e112391a5c4960e224836dba0
Vladyslav Formaliuk <formaliuk@outlook.com>
2024-11-22 17:29:25 +0200

    Fixed logout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ export default async function Home() {
             <form
               action={async () => {
                 'use server';
-                await signOut();
+                await signOut({callbackUrl: '/'});
               }}
             >
               <IconButton icon="/assets/images/logout.svg" label="Logout" />
